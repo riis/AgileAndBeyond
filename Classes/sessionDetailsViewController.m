c304ae50ad153a37113275dfdc9451f72ec1c921
Derek VerLee <dverlee@riis.com>
2011-02-03 14:29:00 -0500

    People rows listed

--- Classes/sessionDetailsViewController.m
+++ Classes/sessionDetailsViewController.m
@@ -9,9 +9,9 @@
 #import  "sessionDetailsViewController.h"
 #import "AgileAndBeyondAppGlobals.h"
 
-
+static const int rowsBeforePeople = 1;
+static const int rowsAfterPeople = 2;
 @implementation sessionDetailsViewController
-
 @synthesize mySession;
 
 #pragma mark -
@@ -84,60 +84,78 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section 
 {
   // Return the number of rows in the section.  
-  return 3;
+   return  [[mySession objectForKey:@"people"]count] + rowsBeforePeople + rowsAfterPeople;
 }
 
 
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath 
 {
-    
+    NSArray* people = [mySession objectForKey:@"people"];
+    const int headcount = [people count];
+    const int i = [indexPath indexAtPosition:1];  // index pos 1, not zero, only 
     static NSString *CellIdentifier = @"Cell";
-    NSArray* people = nil;
-
+ 
     // TODO : fix cell recycling
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
       cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier] autorelease];
     }
 
+    
 
     // Configure the cell...
-
-
     cell.detailTextLabel.lineBreakMode=UILineBreakModeWordWrap;
     cell.detailTextLabel.numberOfLines=0;
     cell.detailTextLabel.font = [UIFont fontWithName:@"Helvetica" size:10.0];
     
-    switch( [indexPath indexAtPosition:1] ) // index pos 1, not zero, only 
+    if( i < rowsBeforePeople )
       {
-      case 0 : 
 	cell.textLabel.text = @"Title";
 	cell.detailTextLabel.text = [mySession objectForKey:@"title"];
-	break;
+      }
+    else if ( i >= headcount + rowsBeforePeople )
+      {
+	switch ( i - (headcount + rowsBeforePeople) )
+	  {
+	  case 0 : 
+	    cell.textLabel.text = @"Schedule";
+	    cell.detailTextLabel.text = 
+	      [[mySession objectForKey:@"timeStart"]
+		descriptionWithCalendarFormat:DATE_FORMAT_STRING timeZone:nil 
+		locale:[[NSUserDefaults standardUserDefaults] dictionaryRepresentation]];
+	    break;
+	  case 1 : 
+	    cell.textLabel.text = @"Description";
+	    cell.detailTextLabel.text = [mySession objectForKey:@"description"];
+	    break;
+	  default : 
+	    // error? 
+	    cell.textLabel.text = @"x";
+	    cell.detailTextLabel.text = @"x";
+	  }
+      }
+    else if ( i >= rowsBeforePeople  && i < (headcount + rowsBeforePeople))
+      {
+	    cell.textLabel.text =
+	      [[[mySession objectForKey:@"people"] objectAtIndex:i-rowsBeforePeople] objectForKey:@"role"];
+	    cell.detailTextLabel.text = 
+	      [[[mySession objectForKey:@"people"] objectAtIndex:i-rowsBeforePeople] objectForKey:@"individual"];
+	    // TODO: conditional disclosure indicator if bio exists..
+	    cell.accessoryType=UITableViewCellAccessoryDisclosureIndicator;    
+	    //	    break;cell.textLabel.text = @"Placeholder";
+	    //cell.detailTextLabel.text = [mySession objectForKey:@"placeholder"];
+      }
+    else NSLog(@"in %s : warning, unreachable code reached.", __func__);
+
+    /*
+      }
       case 1 : 
-	people = [mySession objectForKey:@"people"];
 	if( people && [people count] > 0)
 	  {
-	    cell.textLabel.text = @"Presetor";
-	    cell.detailTextLabel.text = 
-	      [[[mySession objectForKey:@"people"] objectAtIndex:0] objectForKey:@"individual"];
-	    // TODO: conditional disclosure indicator if bio exists..
-	    cell.accessoryType=UITableViewCellAccessoryDisclosureIndicator;	    
-	    break;
+	
 	  } 
-      case 2 : 
-	cell.textLabel.text = @"Schedule";
-	cell.detailTextLabel.text = 
-	  [[mySession objectForKey:@"timeStart"]
-	    descriptionWithCalendarFormat:DATE_FORMAT_STRING timeZone:nil 
-	    locale:[[NSUserDefaults standardUserDefaults] dictionaryRepresentation]];
-	break;
-      case 3 : 
-	cell.textLabel.text = @"Description";
-	cell.detailTextLabel.text = [mySession objectForKey:@"description"];
-	break;
-      }
+    */
       
     return cell;
 }
