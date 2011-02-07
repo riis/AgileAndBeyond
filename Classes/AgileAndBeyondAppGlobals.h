f796a853cbbd50b8cac43429bd730da17ffcc42e
Derek VerLee <dverlee@riis.com>
2011-02-07 15:57:04 -0500

    Add globals for other event dates (or times, less technically)

--- Classes/AgileAndBeyondAppGlobals.h
+++ Classes/AgileAndBeyondAppGlobals.h
@@ -11,6 +11,11 @@
 
 #define AAB_FIRST_SLOT_DATE [NSDate dateWithString:@"2011-03-12 10:15:00 -0500"]
 #define AAB_SECOND_SLOT_DATE [NSDate dateWithString:@"2011-03-12 12:30:00 -0500"]
+#define AAB_WELCOMEINTRO_DATE [NSDate dateWithString:@"2011-03-12 8:30:00 -0500"]
+#define AAB_OPENINGKEYNOTE_DATE [NSDate dateWithString:@"2011-03-12 8:45:00 -0500"]
+#define AAB_CLOSINGKEYNOTE_DATE [NSDate dateWithString:@"2011-03-12 3:15:00 -0500"]
+#define AAB_CLOSINGSUMMARY_DATE [NSDate dateWithString:@"2011-03-12 4:15:00 -0500"]
+
 // date format string is in the old style for date format strings
 // there is a new style that is used with some (all?) functions
 // old style works with descriptionWithCalandarFormat:timezone:locale:dict...
