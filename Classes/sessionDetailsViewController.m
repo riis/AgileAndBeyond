fd3b0fd661a90149086600a078cea4774ad566dd
Derek VerLee <dverlee@riis.com>
2011-02-24 15:27:00 -0500

    clean up code add/remove button refreshed when data changes

--- Classes/sessionDetailsViewController.m
+++ Classes/sessionDetailsViewController.m
@@ -36,139 +36,45 @@ static const int rowsAfterPeople = 2;
 {
   [super viewDidLoad];
   
-  // ============ code from apple example
+  UIGestureRecognizer *recognizer;
 
-    /*
+  recognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeFrom:)];
+  [self.tableView addGestureRecognizer:recognizer];
+  [recognizer release];
 
-     Create and configure the four recognizers. Add each to the view as a gesture recognizer.
+  recognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeFrom:)];
+  self.swipeLeftRecognizer = (UISwipeGestureRecognizer *)recognizer;
+  swipeLeftRecognizer.direction = UISwipeGestureRecognizerDirectionLeft;
+  [self.view addGestureRecognizer:swipeLeftRecognizer];
+  self.swipeLeftRecognizer = (UISwipeGestureRecognizer *)recognizer;
+  [recognizer release];
 
-     */
-
-    UIGestureRecognizer *recognizer;
-
-    
-
-    /*
-
-     Create a tap recognizer and add it to the view.
-
-     Keep a reference to the recognizer to test in gestureRecognizer:shouldReceiveTouch:.
-
-     */
-    /*
-    recognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapFrom:)];
-
-    [self.view addGestureRecognizer:recognizer];
-
-        self.tableView.tapRecognizer = (UITapGestureRecognizer *)recognizer;
-
-    recognizer.delegate = self;
-
-    [recognizer release];
-*/
- 
-
-    /*
-
-     Create a swipe gesture recognizer to recognize right swipes (the default).
-
-     We're only interested in receiving messages from this recognizer, and the view will take ownership of it, so we don't need to keep a reference to it.
-
-     */
-
-    recognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeFrom:)];
-
-    [self.tableView addGestureRecognizer:recognizer];
-
-    [recognizer release];
-
-    
-
-    /*
-
-     Create a swipe gesture recognizer to recognize left swipes.
-
-     Keep a reference to the recognizer so that it can be added to and removed from the view in takeLeftSwipeRecognitionEnabledFrom:.
-
-     Add the recognizer to the view if the segmented control shows that left swipe recognition is allowed.
-
-     */
-
-    recognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeFrom:)];
-
-    self.swipeLeftRecognizer = (UISwipeGestureRecognizer *)recognizer;
-
-    swipeLeftRecognizer.direction = UISwipeGestureRecognizerDirectionLeft;
-
-    
-
-    if ([segmentedControl selectedSegmentIndex] == 0) {
-
-        [self.view addGestureRecognizer:swipeLeftRecognizer];
-
-    }
-
-    self.swipeLeftRecognizer = (UISwipeGestureRecognizer *)recognizer;
-
-    [recognizer release];
-
- 
-
-    /*
-
-     Create a rotation gesture recognizer.
-
-     We're only interested in receiving messages from this recognizer, and the view will take ownership of it, so we don't need to keep a reference to it.
-
-     */
-
-    recognizer = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotationFrom:)];
-
-    [self.view addGestureRecognizer:recognizer];
-
-    [recognizer release];
-
-    
-
-    // For illustrative purposes, set exclusive touch for the segmented control (see the ReadMe).
-
-    [segmentedControl setExclusiveTouch:YES];
-
-    
-
-
-  // ==================================
-
-  // TODO release addRemoveButton? an example online did a release on it in this function.
-}
-
-- (void)viewDidUnload {
-
-    [super viewDidUnload];
-
-    
-
-    self.segmentedControl = nil;
-
-    //    self.tapRecognizer = nil;
-
-    self.swipeLeftRecognizer = nil;
-
-    //    self.imageView = nil;
-
+  // TODO release addRemoveButton? an example online did a release on it in this function.}
 }
 
 
-  - (void)viewWillAppear:(BOOL)animated 
+- (void)viewDidUnload 
+{
+  [super viewDidUnload];
+  self.segmentedControl = nil;
+  self.swipeLeftRecognizer = nil;
+}
+
+
+- (void)viewWillAppear:(BOOL)animated 
 {
   [super viewWillAppear:animated];
-  NSString * buttonTitle;
+  [self refreshButton];
+}
 
-  // TODO make a session method isSelectable
+- (void)refreshButton
+{
+  
+  NSString * buttonTitle;  
   if( mySession.isSelectable )
     {
       buttonTitle = mySession.isUserSelected?@"Remove":@"Add";
-      
+	
       // build the right add/remove button
       UIBarButtonItem *addRemoveButton = [[UIBarButtonItem alloc] 
 					   initWithTitle:buttonTitle
@@ -176,8 +82,7 @@ static const int rowsAfterPeople = 2;
 					   target:self
 					   action:@selector(addToUserSelections)];
       self.navigationItem.rightBarButtonItem = addRemoveButton;
-    }
-
+    }  // TODO release addRemoveButton? an example online did a release on it in this function. 
 }
 
 /*
@@ -377,7 +282,7 @@ return (interfaceOrientation == UIInterfaceOrientationPortrait);
 - (void)dealloc 
 {
   // TODO memory
-  [mySession release];
+  self.mySession=nil;
 
   [super dealloc];
 }
@@ -385,125 +290,13 @@ return (interfaceOrientation == UIInterfaceOrientationPortrait);
 
 //*****************+++++++++++++++++_____________________------------------
 
- 
-
- 
-/*
-- (IBAction)takeLeftSwipeRecognitionEnabledFrom:(UISegmentedControl *)aSegmentedControl {
-
- 
-
-  //     Add or remove the left swipe recogniser to or from the view depending on the selection in the segmented control.
-
-    
-    if ([aSegmentedControl selectedSegmentIndex] == 0) {
-        [self.view addGestureRecognizer:swipeLeftRecognizer];
-    }
-
-    else {
-        [self.view removeGestureRecognizer:swipeLeftRecognizer];
-    }
-
-}
-*/
- 
-
- 
-
-- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
-
- 
-
-    // Disallow recognition of tap gestures in the segmented control.
-
-  if ((touch.view == segmentedControl)/* && (gestureRecognizer == tapRecognizer)*/) {
-
-        return NO;
-
-    }
-
-    return YES;
-
-}
-
- 
-
- 
-
 #pragma mark -
-
 #pragma mark Responding to gestures
 
- 
 
-- (void)showImageWithText:(NSString *)string atPoint:(CGPoint)centerPoint {
-
-    
-  NSLog(@"hello from %s", __func__);
-
-    /*
-
-     Set the appropriate image for the image view, move the image view to the given point, then dispay it by setting its alpha to 1.0.
-
-     */
-
-  //    NSString *imageName = [string stringByAppendingString:@".png"];
-
-  //imageView.image = [UIImage imageNamed:imageName];
-
-  //imageView.center = centerPoint;
-
-  //imageView.alpha = 1.0;  
-
-}
-
- 
-
-/*
-
- In response to a tap gesture, show the image view appropriately then make it fade out in place.
-
- */
-
-- (void)handleTapFrom:(UITapGestureRecognizer *)recognizer {
-
-
-  NSLog(@"hello from %s", __func__);    
-
-  //    CGPoint location = [recognizer locationInView:self.view];
-    /*
-    [self showImageWithText:@"tap" atPoint:location];
-
-    
-
-    [UIView beginAnimations:nil context:NULL];
-
-    [UIView setAnimationDuration:0.5];
-
-    imageView.alpha = 0.0;
-
-    [UIView commitAnimations];
-    */
-}
-
- 
-
-/*
-
- In response to a swipe gesture, show the image view appropriately then move the image view in the direction of the swipe as it fades out.
-
- */
-
-- (void)handleSwipeFrom:(UISwipeGestureRecognizer *)recognizer {
-
- 
+- (void)handleSwipeFrom:(UISwipeGestureRecognizer *)recognizer 
+{
   BUGOUT(@"hello from %s", __func__);
-  
-  
-  
-  //    [self showImageWithText:@"swipe" atPoint:location];
-  
-  
   
   if (recognizer.direction == UISwipeGestureRecognizerDirectionLeft) 
     {
@@ -512,6 +305,7 @@ return (interfaceOrientation == UIInterfaceOrientationPortrait);
       int indexOfCurrent = [AABSessions indexOfObject:mySession];
       mySession = (indexOfCurrent==0)?[AABSessions lastObject]:[AABSessions objectAtIndex:indexOfCurrent-1];
       [self.tableView reloadData];
+      [self refreshButton];
     }
   else 
     {
@@ -521,61 +315,10 @@ return (interfaceOrientation == UIInterfaceOrientationPortrait);
 	      [AABSessions objectAtIndex:0]
 	      :[AABSessions objectAtIndex:1+[AABSessions indexOfObject:mySession]];
       [self.tableView reloadData];
+      [self refreshButton];
     }
   
-    
-    /*
-    [UIView beginAnimations:nil context:NULL];
-
-    [UIView setAnimationDuration:0.55];
-
-    imageView.alpha = 0.0;
-
-    imageView.center = location;
-
-    [UIView commitAnimations];
-    */
 }
-
- 
-
-/*
-
- In response to a rotation gesture, show the image view at the rotation given by the recognizer, then make it fade out in place while rotating back to horizontal.
-
- */
-
-- (void)handleRotationFrom:(UIRotationGestureRecognizer *)recognizer {
-
-    
-  /*
-    CGPoint location = [recognizer locationInView:self.view];
-
-    
-
-    CGAffineTransform transform = CGAffineTransformMakeRotation([recognizer rotation]);
-
-    imageView.transform = transform;
-
-    [self showImageWithText:@"rotation" atPoint:location];
-
-    
-
-    [UIView beginAnimations:nil context:NULL];
-
-    [UIView setAnimationDuration:0.65];
-
-    imageView.alpha = 0.0;
-
-    imageView.transform = CGAffineTransformIdentity;
-
-    [UIView commitAnimations];
-  */
-
-  NSLog(@"hello from %s", __func__);
-}
-
- 
 
 @end
 
